ebd1b2b07d6055fe10a8fba8fdc56e4b4ffe54f8
Mafe Ospina <mafe.salinas2040@gmail.com>
2023-05-16 19:44:06 -0500

    parametros sonarqube

--- src/com/devops/sonarqube.groovy
+++ src/com/devops/sonarqube.groovy
@@ -1,10 +1,5 @@
 package com.devops
 
-/*def hello(Map params){
-    echo "Hello Laura cucos sucios y valentina cagona ${params.par1}"
-    echo "Hello Laura cucos sucios y valentina cagona ${params.par2}"   
-}*/
-
 def scanner(Map params){
 
     def scannerHome = tool "${params.scannerHome}" 
@@ -13,7 +8,7 @@ def scanner(Map params){
         -Dsonar.projectKey=${params.key} \
         -Dsonar.projectName=${params.name} \
         -Dsonar.projectVersion=${params.version} \
-        -Dsonar.sources=/var/jenkins_home/workspace/${params.name} \
+        -Dsonar.sources=/var/jenkins_home/workspace/Reto_1 \
         -Dsonar.host.url=http://sonarqube:9000\
         -Dsonar.login=admin \
         -Dsonar.password=admin1 "
