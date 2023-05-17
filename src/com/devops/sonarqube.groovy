0f0379bd12bf1e77ccc89d45261f8b2ff19d16a6
Mafe Ospina <mafe.salinas2040@gmail.com>
2023-05-16 19:14:10 -0500

    parametros

--- src/com/devops/sonarqube.groovy
+++ src/com/devops/sonarqube.groovy
@@ -2,5 +2,5 @@ package com.devops
 
 def hello(Map params){
     echo "Hello Laura cucos sucios y valentina cagona ${params.par1}"
-    echo "Hello Laura cucos sucios y valentina cagona ${params.par2}"
+    echo "Hello Laura cucos sucios y valentina cagona ${params.par2}"   
 }
