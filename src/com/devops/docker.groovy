ca2a59b48919db04bf52b07842d766e24ddf8c6e
Maria_Ospina <mariaospina@cbit-online.com>
2023-05-30 15:35:00 -0500

    push docker

--- src/com/devops/docker.groovy
+++ src/com/devops/docker.groovy
@@ -1,12 +1,12 @@
 package com.devops
 
 def buildimage(repo_name){
-    sh "docker build -t ${repo_name}:${env.BUILD_ID} ."
+    sh "docker build -t mafe2/${repo_name}:${env.BUILD_ID} ."
 }
 
 def pushimage(repo_name){
     withDockerRegistry([ credentialsId: "dockerhub", url: "https://index.docker.io/v1/" ]) {
-        sh "docker push ${repo_name}:${env.BUILD_ID}"
+        sh "docker push mafe2/${repo_name}:${env.BUILD_ID}"
     }
 }
 
