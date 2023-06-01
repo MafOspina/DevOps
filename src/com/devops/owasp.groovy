67005d01013e06d6a45b01e15e7f13c636fbb16b
Maria_Ospina <mariaospina@cbit-online.com>
2023-06-01 12:59:39 -0500

    prueba owasp

--- src/com/devops/owasp.groovy
+++ src/com/devops/owasp.groovy
@@ -2,6 +2,9 @@ package com.devops
 
 def scanOwasp(repo_name){
     sh 'docker run -dt --name owasp -v owasp_data:/zap/reports --user root -t owasp/zap2docker-stable /bin/bash'
+    sh 'docker network create app2'
+    sh 'docker network connect app2 owasp'
+    sh 'docker network connect app2 app2'
     sh 'docker exec owasp mkdir /zap/wrk'
     sh 'docker exec owasp zap-full-scan.py -t http://app2:8888/ -r report.html -I'
     sh 'docker cp owasp:/zap/wrk/report.html report.html'
