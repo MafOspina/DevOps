3ba793067dec56072c2225ee64d69dd31fb33aef
Maria_Ospina <mariaospina@cbit-online.com>
2023-06-05 19:32:12 -0500

    prueba condicional contenedor

--- src/com/devops/docker.groovy
+++ src/com/devops/docker.groovy
@@ -13,22 +13,13 @@ def pushimage(repo_name){
 def deployimage(repo_name){
     
     def validacion_cont = sh(returnStdout: true, script: 'echo "$(docker ps -q --filter name=${repo_name})"').trim()
-                        
+
+    echo "el contenedor es ${repo_name}"                        
     if (validacion_cont != '') {  
-        /*sh "docker stop ${repo_name}"
-        sh "docker rm ${repo_name}"*/
-        echo "el contenido es: ${validacion_cont}"
+        sh "docker stop ${repo_name}"
+        sh "docker rm ${repo_name}"
     } 
 
     //sh "docker run -d --name ${repo_name} -p 8888:8888 mafe2/${repo_name}:${env.BUILD_ID}"
-
-
-    def containerExists = "docker ps -a --format '{{.Names}}' | grep $repo_name"
-
-    if (containerExists.execute().text.contains(repo_name)) {
-        println "El contenedor $repo_name existe."
-    } else {
-        println "El contenedor $containerExists no existe."
-    }
 }
 
