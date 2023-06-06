316d5ce2fbfbfee54bb6b974e4270165c7c4b0eb
Maria_Ospina <mariaospina@cbit-online.com>
2023-06-06 10:11:03 -0500

    prueba owasp

--- src/com/devops/owasp.groovy
+++ src/com/devops/owasp.groovy
@@ -8,8 +8,8 @@ def scanOwasp(repo_name){
         sh 'docker exec owasp mkdir /zap/wrk'
     } 
     
-    /*sh 'docker exec owasp zap-full-scan.py -t http://app2:8888/ -r report.html -I'
+    sh 'docker exec owasp zap-full-scan.py -t http://app2:8888/ -r report.html -I'
     sh 'docker cp owasp:/zap/wrk/report.html report.html'
-    sh 'docker cp report.html jenkins:/var/jenkins_home/workspace/Aplicacion-reto/'*/
+    sh 'docker cp report.html jenkins:/var/jenkins_home/workspace/Aplicacion-reto/'
 }
 
